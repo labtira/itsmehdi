c8bede8f46c1b586e5e6e99d37737bb5c15666ac
Mehdi <labtira.dev@gmail.com>
2025-03-12 21:58:11 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -151,7 +151,7 @@ function Home() {
               onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
               className="px-6 py-2 bg-gradient-to-r from-[#111111] to-[#1A1A1A] rounded-lg flex items-center gap-3 hover:from-[#1A1A1A] hover:to-[#222222] transition-all text-md border border-[#222222] hover:border-[#333333] shadow-md group focus:outline-none"
             >
-              View All Projects
+              More
               <svg className="w-5 h-5 lg:w-6 lg:h-6 transform group-hover:translate-x-1 transition-transform" viewBox="0 0 24 24" fill="none">
                 <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
