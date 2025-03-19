39a18c0f3baed9e4ba41aa78215877b07b192d4c
Mehdi Labtira <labtira.dev@gmail.com>
2025-03-19 00:27:51 +0000

    Update LogoNavbar.jsx

--- src/components/LogoNavbar.jsx
+++ src/components/LogoNavbar.jsx
@@ -6,11 +6,11 @@ function LogoNavbar() {
   const isHomePage = location.pathname === '/';
 
   return (
-    <div className="fixed top-0 right-0 left-0 z-50 py-4 px-7 ">
+    <div className="fixed top-0 right-0 left-0 z-50 py-4 px-8 ">
      <Link 
             to="/" 
             >
-             <div className='w-full rounded-[16px] flex justify-center items-center transition-all duration-500 backdrop-blur-xl bg-gradient-to-b from-black/10 to-black/30 border  border-[#222222] shadow-[0_-8px_32px_rgba(0,0,0,0.12)] py-4 px-6 md:mx-auto md:max-w-screen-sm'>
+             <div className='w-full rounded-[16px] flex justify-center items-center transition-all duration-500 backdrop-blur-xl bg-gradient-to-b from-black/10 to-black/30 border  border-[#222222] shadow-[0_-8px_32px_rgba(0,0,0,0.12)] py-4  md:mx-auto md:max-w-screen-sm'>
           <img src={logo1} alt="Logo" className="h-10" />
           </div>
           </Link>
